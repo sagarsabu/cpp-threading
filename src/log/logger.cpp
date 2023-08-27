391cab022cf708ca9efd04f43735d0824f72b725
Sagar Sabu <chelekadusagar@gmail.com>
2023-08-27 16:23:25 +1200

    Add additional logging levels

--- src/log/logger.cpp
+++ src/log/logger.cpp
@@ -5,56 +5,199 @@
 #include <mutex>
 #include <sstream>
 #include <iomanip>
+#include <unordered_map>
 
 #include "log/logger.hpp"
 
 namespace Sage::Log
 {
 
-constexpr size_t MAX_LOG_BUFF_SIZE = 1024;
-constexpr size_t MILLI_SEC_BUFF_SIZE = 16;
+// Aliases
+
+using MsgBuffer = char[1024];
+using MilliSecBuffer = char[16];
+
+// Formatter control
+
+static const char FORMAT_END[] = "\x1B[00m";
+static const char FORMAT_BOLD[] = "\x1B[01m";
+static const char FORMAT_DISABLED[] = "\x1B[02m";
+static const char FORMAT_ITALIC[] = "\x1B[03m";
+static const char FORMAT_URL[] = "\x1B[04m";
+static const char FORMAT_BLINK[] = "\x1B[05m";
+static const char FORMAT_BLINK2[] = "\x1B[06m";
+static const char FORMAT_SELECTED[] = "\x1B[07m";
+static const char FORMAT_INVISIBLE[] = "\x1B[08m";
+static const char FORMAT_STRIKE[] = "\x1B[09m";
+static const char FORMAT_DOUBLE_UNDERLINE[] = "\x1B[21m";
+
+// Dark Colours
+
+static const char DARK_BLACK[] = "\x1B[30m";
+static const char DARK_RED[] = "\x1B[31m";
+static const char DARK_GREEN[] = "\x1B[32m";
+static const char DARK_YELLOW[] = "\x1B[33m";
+static const char DARK_BLUE[] = "\x1B[34m";
+static const char DARK_VIOLET[] = "\x1B[35m";
+static const char DARK_BEIGE[] = "\x1B[36m";
+static const char DARK_WHITE[] = "\x1B[37m";
+
+// Light Colours
+
+static const char LIGHT_GREY[] = "\x1B[90m";
+static const char LIGHT_RED[] = "\x1B[91m";
+static const char LIGHT_GREEN[] = "\x1B[92m";
+static const char LIGHT_YELLOW[] = "\x1B[93m";
+static const char LIGHT_BLUE[] = "\x1B[94m";
+static const char LIGHT_VIOLET[] = "\x1B[95m";
+static const char LIGHT_BEIGE[] = "\x1B[96m";
+static const char LIGHT_WHITE[] = "\x1B[97m";
+
+// Globals
+
+static const std::unordered_map<Level, const char*> g_levelColour
+{
+    {Level::Debug,      DARK_BLUE},
+    {Level::Info,       DARK_WHITE},
+    {Level::Warning,    LIGHT_YELLOW},
+    {Level::Error,      LIGHT_RED},
+    {Level::Critical,   DARK_RED},
+};
+
+static const std::unordered_map<Level, const char*> g_levelInfo
+{
+    {Level::Debug,      "DEBUG"},
+    {Level::Info,       "INFO "},
+    {Level::Warning,    "WARN "},
+    {Level::Error,      "ERROR"},
+    {Level::Critical,   "CRIT "},
+};
+
 std::mutex g_logMutex;
+
+Level g_currentLogLevel{ Level::Debug };
+
+// Helper classes / structs
 
 struct LogTimestamp
 {
-    char m_msSecBuff[MILLI_SEC_BUFF_SIZE];
-    ::timespec m_ts;
-};
-
-LogTimestamp GetTimestamp()
-{
-    LogTimestamp timestamp;
-    ::clock_gettime(CLOCK_REALTIME, &timestamp.m_ts);
-
-    long millisec = timestamp.m_ts.tv_nsec / 1000000;
-    // incase of overflow
-    if (millisec >= 1000)
+    LogTimestamp()
     {
-        millisec -= 1000;
-        timestamp.m_ts.tv_sec++;
+        ::clock_gettime(CLOCK_REALTIME, &m_timeSpec);
+
+        long millisec = m_timeSpec.tv_nsec / 1000000;
+        // incase of overflow
+        if (millisec >= 1000)
+        {
+            millisec -= 1000;
+            m_timeSpec.tv_sec++;
+        }
+
+        snprintf(m_msSecBuff, sizeof(m_msSecBuff), ":%03ld", millisec);
     }
 
-    snprintf(timestamp.m_msSecBuff, sizeof(timestamp.m_msSecBuff), ":%03ld", millisec);
+    inline const time_t& getSeconds() const { return m_timeSpec.tv_sec; }
 
-    return timestamp;
+    inline const time_t& getNanoSeconds() const { return m_timeSpec.tv_nsec; }
+
+    inline const MilliSecBuffer& getMilliSecBuffer() const { return m_msSecBuff; };
+
+private:
+    MilliSecBuffer m_msSecBuff;
+    ::timespec m_timeSpec;
+};
+
+// Functions
+
+const char* getLevelFormatter(Level level) { return g_levelColour.at(level); }
+
+const char* getLevelInfo(Level level) { return g_levelInfo.at(level); }
+
+void SetLogLevel(Level logLevel) { g_currentLogLevel = logLevel; }
+
+void LogToStdOut(Level logLevel, const MsgBuffer& msgBuff)
+{
+    LogTimestamp ts;
+
+    {
+        std::scoped_lock lock{ g_logMutex };
+        std::cout
+            << getLevelFormatter(logLevel)
+            << '[' << std::put_time(std::localtime(&ts.getSeconds()), "%d-%m-%Y %H:%M:%S") << ts.getMilliSecBuffer() << "] "
+            << '[' << getLevelInfo(logLevel) << "] "
+            << msgBuff
+            << FORMAT_END << '\n';
+    }
 }
 
-void info(const char* msg, ...)
+void Debug(const char* msg, ...)
 {
-    char msgBuff[MAX_LOG_BUFF_SIZE];
-    LogTimestamp timestamp{ GetTimestamp() };
+    if (Level::Debug < g_currentLogLevel)
+        return;
 
+    MsgBuffer msgBuff;
     va_list args;
     va_start(args, msg);
     vsnprintf(msgBuff, sizeof(msgBuff), msg, args);
     va_end(args);
 
-    {
-        std::scoped_lock<std::mutex> logGuard{ g_logMutex };
-        std::cout
-            << '[' << std::put_time(std::localtime(&timestamp.m_ts.tv_sec), "%d-%m-%Y %H:%M:%S") << timestamp.m_msSecBuff << "] "
-            << msgBuff << '\n';
-    }
+    LogToStdOut(Level::Debug, msgBuff);
+}
+
+void Info(const char* msg, ...)
+{
+    if (Level::Info < g_currentLogLevel)
+        return;
+
+    MsgBuffer msgBuff;
+    va_list args;
+    va_start(args, msg);
+    vsnprintf(msgBuff, sizeof(msgBuff), msg, args);
+    va_end(args);
+
+    LogToStdOut(Level::Info, msgBuff);
+}
+
+void Warning(const char* msg, ...)
+{
+    if (Level::Warning < g_currentLogLevel)
+        return;
+
+    MsgBuffer msgBuff;
+    va_list args;
+    va_start(args, msg);
+    vsnprintf(msgBuff, sizeof(msgBuff), msg, args);
+    va_end(args);
+
+    LogToStdOut(Level::Warning, msgBuff);
+}
+
+void Error(const char* msg, ...)
+{
+    if (Level::Error < g_currentLogLevel)
+        return;
+
+    MsgBuffer msgBuff;
+    va_list args;
+    va_start(args, msg);
+    vsnprintf(msgBuff, sizeof(msgBuff), msg, args);
+    va_end(args);
+
+    LogToStdOut(Level::Error, msgBuff);
+}
+
+void Critical(const char* msg, ...)
+{
+    if (Level::Critical < g_currentLogLevel)
+        return;
+
+    MsgBuffer msgBuff;
+    va_list args;
+    va_start(args, msg);
+    vsnprintf(msgBuff, sizeof(msgBuff), msg, args);
+    va_end(args);
+
+    LogToStdOut(Level::Critical, msgBuff);
 }
 
 } // namespace Sage::Log
--- src/log/logger.hpp
+++ src/log/logger.hpp
@@ -1,9 +1,50 @@
 #pragma once
 
+#include <functional>
+
 namespace Sage::Log
 {
 
+enum class Level
+{
+    Debug,
+    Info,
+    Warning,
+    Error,
+    Critical
+};
+
+constexpr bool operator<(Level lhs, Level rhs)
+{
+    return (static_cast<uint8_t>(lhs) < static_cast<uint8_t>(rhs));
+}
+
+void SetLogLevel(Level logLevel);
+
 [[gnu::format(printf, 1, 2)]]
-void info(const char* msg, ...);
+void Debug(const char* msg, ...);
+
+[[gnu::format(printf, 1, 2)]]
+void Info(const char* msg, ...);
+
+[[gnu::format(printf, 1, 2)]]
+void Warning(const char* msg, ...);
+
+[[gnu::format(printf, 1, 2)]]
+void Error(const char* msg, ...);
+
+[[gnu::format(printf, 1, 2)]]
+void Critical(const char* msg, ...);
 
 } // namespace Sage::Log
+
+// Template specialization for unordered_collections<Level>
+template<>
+struct std::hash<Sage::Log::Level>
+{
+    size_t operator()(Sage::Log::Level level) const noexcept
+    {
+        size_t hVal = std::hash<size_t>{}(static_cast<size_t>(level));
+        return hVal;
+    }
+};
